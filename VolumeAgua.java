bd6e13ab32ac01ec0e2a12f07e203efefcb33d69
Elisangela Rocha <elisangelarocha.ti@gmail.com>
2026-05-26 20:51:24 -0300

    Calculo do Volume de Agua

---
+++ VolumeAgua.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class VolumeAgua {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int volumeDia;
+        int total = 0;
+
+        // Laço para os 5 dias
+        for (int dia = 1; dia <= 5; dia++) {
+            System.out.print("Digite o volume de água bombeada no dia " + dia + " (em litros): ");
+            volumeDia = scanner.nextInt();
+
+            total += volumeDia; // soma ao total
+        }
+
+        // Resultado final
+        System.out.println("\nVolume total distribuído no período: " + total + " litros");
+
+        scanner.close();
+    }
+}
+
